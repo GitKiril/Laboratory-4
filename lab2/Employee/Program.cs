683c8a2448d23c75ffd96b570888ec22488d808c
Kiril <kiril.strykalenko@ilcoud.com>
2022-09-19 15:04:08 +0300

    laboratorna2

---
+++ lab2/Employee/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Employee
+{
+    public class Employee
+    {
+        private readonly string name;
+        private readonly string secondname;
+
+        public Employee(string name, string secondname)
+        {
+            this.name = name;
+            this.secondname = secondname;
+        }
+
+        public void salary()
+        {
+            int position, experience;
+            double salary, tax;
+            Console.WriteLine("We have three positions and types of experience");
+            Console.WriteLine("Choice position");
+            position = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choice experience");
+            experience = Convert.ToInt32(Console.ReadLine());
+            if (position == 1 && experience <= 3)
+            {
+                Console.WriteLine("You office manager");
+                salary = ((50000 + 50000) / 2);
+                tax = salary * 0.18;
+                Console.WriteLine($"Your salary: {salary}");
+                Console.WriteLine($"Your tax: {tax}");
+            }
+            if (position == 2 && experience <= 3)
+            {
+                Console.WriteLine("You accountant");
+                salary = ((60000 + 23000) / 2);
+                tax = salary * 0.18;
+                Console.WriteLine($"Your salary: {salary}");
+                Console.WriteLine($"Your tax: {tax}");
+            }
+            if (position == 3 && experience >= 10)
+            {
+                Console.WriteLine("You head director");
+                salary = ((80000 + 84000) / 2);
+                tax = salary * 0.18;
+                Console.WriteLine($"Your salary: {salary}");
+                Console.WriteLine($"Your salary: {tax}");
+            }
+
+            Console.WriteLine($"You {name} {secondname} and your posiotion {position}.");
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Ivan", "Kulic");
+            employee.salary();
+        }
+    }
+}
---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Address
+{
+    public class Address
+    {
+
+        public int Index { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public string House { get; set; }
+
+        public string Apartment { get; set; }
+
+
+        public void DisplayName()
+        {
+            Console.WriteLine($"Index: {Index} \fCountry : {Country} \fCity : {City} \fStreet : {Street} \fHouse : {House} \fApartment : {Apartment}");
+        }
+
+        static void Main()
+        {
+            Address address = new Address();
+            address.Index = 02000;
+            address.Country = "Ukraine";
+            address.City = "Kyiv";
+            address.Street = "Lobachevskogo 23";
+            address.House = "6";
+            address.Apartment = "721";
+            address.DisplayName();
+        }
+    }
+}
